fedff7d2a603da28812630f39e924ec0de1c4d63
Gustavo Bispo <38411510+gustavolbs@users.noreply.github.com>
2022-11-13 09:14:39 -0300

    fix eslint and jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
+/** @type {import('jest').Config} */
 module.exports = {
+  verbose: true,
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/build/'],
   transform: {
     '^.+\\.(js|ts)$': '<rootDir>/node_modules/babel-jest',
@@ -7,12 +9,7 @@ module.exports = {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^test/(.*)$': '<rootDir>/test/$1',
   },
-  collectCoverageFrom: [
-    '<rootDir>/src/controllers/**/*.js',
-    '<rootDir>/src/database/service/**/*.js',
-    '<rootDir>/src/middleware/**/*.js',
-    '<rootDir>/src/routes/**/*.js',
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.js'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
